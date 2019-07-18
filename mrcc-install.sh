4f3d994781f33a74bf0e229c1dd9e8db7c40d837
Nircek <nircek-2103@protonmail.com>
2019-07-18 17:42:46 +0200

    Add the verification of system

--- mrcc-install.sh
+++ mrcc-install.sh
@@ -25,17 +25,25 @@
 # SOFTWARE.
 
 choice () {
+  echo -n "$@ [y/n] "
   while [ true ]
   do
     read -rsn1 key
     if [ $key = 'y' ] || [ $key = 'Y' ]
     then
+      echo y
       return 0
     elif [ $key = 'n' ] || [ $key = 'N' ]
     then
+      echo n
       return 1
     fi
   done
+}
+
+choice-no () {
+  choice "$@"
+  [ $? -eq 0 ] && return 1 || return 0
 }
 
 LOG_FILE=log.txt
@@ -51,6 +59,7 @@ log () {
 trace () {
   log "$""$@"
   "$@" 2>&1 | tee -a $LOG_FILE
+  return ${PIPESTATUS[0]}${pipestatus[1]}
 }
 
 text="mrcc-install
@@ -60,11 +69,25 @@ under MIT License
 There is NO WARRANTY, to the extent permitted by law.
 I'm not taking any responsibility for anything that this program does.
 Please use it with caution.
-Do you accept this?
 "
 echo -e "$text"
-choice
-[ $? -eq 1 ] && exit 1
+choice-no "Do you accept this?" && exit 1
 log "Started logging"
+exit () {
+  log "Exit with code: $@"
+  command exit "$@"
+}
 [ -d /sys/firmware/efi ] || { log "This is not EFI. Sorry."; exit 2; }
-trace echo xd
+good="is"
+bad () { good="is NOT"; "$@"; }
+archiso="`find /dev/disk/by-label/ARCH_*  -printf "%f\n" 2>/dev/null`" || { bad; archiso="UNKNOWN"; }
+pacman="`pacman -Q linux 2>/dev/null`" || { bad; linux="NO PACMAN"; }
+[ "`uname -s`" = "Linux" ] && [ "`uname -o`" = "GNU/Linux" ] && linux="`uname -sr`" || { bad; linux="NO LINUX"; }
+name="`uname -n`"
+[ "$name" = "archiso" ] || bad
+log "Your version of system is $archiso."
+log "Found $linux by uname and $pacman by pacman."
+log "The nodename is $name."
+log "I think it $good an iso of Arch Linux."
+[ "$good" != "is" ] && trace choice-no "Do you REALLY want to continue?" && exit 3
+exit 0
